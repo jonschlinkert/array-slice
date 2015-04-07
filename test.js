a9d7ed0bfe7ee4599144e9b9c68c51b62bf65445
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-07 04:55:06 -0400

    run update

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 /*!
  * array-slice <https://github.com/jonschlinkert/array-slice>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT License
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
-var should = require('should');
+require('should');
 var slice = require('./');
 
 describe('array-slice:', function () {
